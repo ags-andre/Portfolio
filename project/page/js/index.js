f4c13726a49752e6f507a6442cea2ac3145d4408
ANDRE GUSTAVO SANTOS <andregustavosantos@policiamilitar.sp.gov.br>
2024-04-16 09:55:01 -0300

    criação da seção contatos e ajustes para codigo limpo

--- project/page/js/index.js
+++ project/page/js/index.js
@@ -17,12 +17,11 @@ function openCloseAccordion(el) {
 function editAccordionHeight(accordion, opened) {
     if(opened) {
         const getStyle = (el) => window.getComputedStyle(el);
-
         const divDescricao = accordion.querySelector('.descricao');
         const divDescricaoMargin = parseInt(getStyle(divDescricao).marginTop.split(1, -2));//slice => remover o 'px'
         const divDescricaoHeight = divDescricao.offsetHeight;
         const soma = [divDescricaoHeight, divDescricaoMargin].reduce((acc, val) => acc + val, 0);
-        
+
         accordion.style.height = `${accordion.offsetHeight + soma}px`;
     } else {
         accordionHeightInicial(accordion);
